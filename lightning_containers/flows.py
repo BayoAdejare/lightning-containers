ef9b52e490626028250103b9d18afb56c5cef200
Bayo <4624500+BayoAdejare@users.noreply.github.com>
2024-02-16 01:40:23 -0500

    refresh; add more points

--- lightning_containers/flows.py
+++ lightning_containers/flows.py
@@ -1,5 +1,5 @@
 from prefect import flow
-from tasks.etl import source, transformations, sink
+from tasks import *
 from tasks.clustering import (
     preprocessor,
     kmeans_cluster,
@@ -7,12 +7,43 @@ from tasks.clustering import (
     elbow_evaluator,
 )
 
+start_date = "02/13/2023"
+end_date = "02/14/2023"
+
+
+# 24 hours :test:
+hours = [
+    "00",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+]
+
 
 @flow(retries=3, retry_delay_seconds=15, log_prints=True)
 def etl_ingest():
-    s = source()
-    t = transformations(s)
-    s = sink(t)
+    print(f"Starting ingestion from {start_date} to {end_date}..")
+    ingestion(start_date=start_date, end_date=end_date, hours=hours)
     return "ETL Flow completed!"
 
 
