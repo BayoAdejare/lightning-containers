f941eb01118a812677dbdd1ec6ee23bfa7935aad
Bayo Adejare <bayo.adejare@hotmail.com>
2024-10-07 10:50:27 -0700

    requirements cosmetic fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="lightning_containers",
     version="0.0.1",
-    packages=find_packages(exclude=["lightning_containers_tests"]),
+    packages=find_packages(exclude=["tests"]),
     install_requires=[
         ## backend dependencies
         "prefect==2.20.9",
@@ -14,11 +14,12 @@ setup(
         "scikit-learn==1.5.0",
         "SQLAlchemy==2.0.25",
         "tqdm==4.66.3",
+        "numpy==1.24.3",  # Add numpy with a specific version
         # frontend dependencies
         "streamlit==1.37.0",
         "plotly==5.18.0",
     ],
     author="Bayo Adejare",
     author_email="4624500+BayoAdejare@users.noreply.github.com",
-    extras_requires={"dev": ["pytest==7.4.2"]},
+    extras_require={"dev": ["pytest==7.4.2"]},
 )
